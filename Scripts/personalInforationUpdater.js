07f8fad2f59ec903aa90b36346d142487027eeaf
toxikdnb <toxikdnb@hotmail.com>
2024-04-12 16:24:34 +0100

    Initial Commit

---
+++ Scripts/personalInforationUpdater.js
@@ -0,0 +1,35 @@
+let fullname = document.getElementById("name");
+let title = document.getElementById("title");
+let pfp = document.getElementById("pfp");
+
+let navbarContainer = document.getElementById("navbarContainer");
+
+fetch("../Scripts/Data/personalInfo.json")
+    .then((response) => response.json())
+    .then((personalInformation) => {
+        fullname.innerHTML = personalInformation.name;
+        title.innerHTML = personalInformation.title;
+        pfp.src = personalInformation.profilePicture;
+        navbarElements = personalInformation["pages"];
+        let pageName = window.location.pathname.split("/").pop();
+        navbarElements.forEach((element) => {
+            let navElement = document.createElement("a");
+            // If link not homepage, add "/Pages/"
+            if (pageName == "index.html") {
+                element.link = "Pages/" + element.link;
+            }
+            else if (element.link == "index.html") {
+                element.link = "../" + element.link;
+            }
+            if (element.link.includes(pageName)) {
+                let div = document.createElement("div");
+                div.classList.add("ActivatedLink");
+                div.innerHTML = element.name;
+                navElement.appendChild(div);
+            } else {
+                navElement.innerHTML = element.name;
+            }
+            navElement.href = element.link;
+            navbarContainer.appendChild(navElement);
+        });
+    });
---
+++ Scripts/qualificationTablePopulator.js
@@ -0,0 +1,29 @@
+let table = document.getElementById("qualificationsTable");
+
+function addQualification(gradeType, qualification) {
+    let row = table.insertRow(-1);
+    let type = row.insertCell(0);
+    let subject = row.insertCell(1);
+    let grade = row.insertCell(2);
+
+    type.innerHTML = gradeType;
+    subject.innerHTML = qualification.subject;
+    grade.innerHTML = qualification.grade;
+}
+
+fetch("../Scripts/Data/qualifications.json")
+    .then((response) => response.json())
+    .then((qualificationsFile) => {
+        for (property in qualificationsFile) {
+            data = qualificationsFile[property];
+            data.forEach((element) => {
+                console.log(element);
+                let key = Object.keys(element)[0];
+                addQualification(property, {
+                    subject: key,
+                    grade: element[key],
+                });
+            });
+        }
+    })
+    .catch((error) => console.error("An error occurred:", error));
