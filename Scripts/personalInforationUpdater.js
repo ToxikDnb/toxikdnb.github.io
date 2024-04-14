d6a9c2a03fe93178621ba7e2ce0e0006c9a4af07
toxikdnb <toxikdnb@hotmail.com>
2024-04-14 16:48:38 +0100

    debug 2

--- Scripts/personalInforationUpdater.js
+++ Scripts/personalInforationUpdater.js
@@ -9,17 +9,25 @@ fetch("/Scripts/Data/personalInfo.json")
     .then((personalInformation) => {
         fullname.innerHTML = personalInformation.name;
         title.innerHTML = personalInformation.title;
-        pfp.src = personalInformation.profilePicture;
+        pfp.src =
+            "https://" +
+            window.Location.hostname +
+            "/" +
+            personalInformation.profilePicture;
         navbarElements = personalInformation["pages"];
         let pageName = window.location.pathname.split("/").pop();
         navbarElements.forEach((element) => {
             let navElement = document.createElement("a");
             console.log(pageName, element.link);
-            console.log(element.link.splot("/").pop());
-            if ((element.link == "" && pageName == element.link) || pageName == element.link.split("/").pop()) {
+            console.log(element.link.split("/").pop());
+            if (
+                (element.link == "" && pageName == element.link) ||
+                pageName == element.link.split("/").pop()
+            ) {
                 navElement.classList.add("ActivatedLink");
             }
-            navElement.href = "https://" + window.location.hostname + "/" + element.link;
+            navElement.href =
+                "https://" + window.location.hostname + "/" + element.link;
             navElement.innerHTML = element.name;
             navbarContainer.appendChild(navElement);
         });
