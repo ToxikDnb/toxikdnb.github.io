18ccca14db261c64f98b14c057bc53e978212c38
toxikdnb <toxikdnb@hotmail.com>
2024-04-15 15:42:09 +0100

    bug

--- Scripts/personalInforationUpdater.js
+++ Scripts/personalInforationUpdater.js
@@ -2,7 +2,11 @@ let fullname = document.getElementById("name");
 let title = document.getElementById("title");
 let pfp = document.getElementById("pfp");
 let navbarContainer = document.getElementById("navbarContainer");
-fetch(window.location.hostname + "/Scripts/Data/personalInfo.json")
+fetch(
+    "https://www." +
+        window.location.hostname +
+        "/Scripts/Data/personalInfo.json"
+)
     .then((response) => response.json())
     .then((personalInformation) => {
         fullname.innerHTML = personalInformation.name;
