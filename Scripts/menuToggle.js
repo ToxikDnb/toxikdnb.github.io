0b83dab736683e2d6fd858d5bc3512b75adf3c84
toxikdnb <toxikdnb@hotmail.com>
2024-04-17 14:42:50 +0100

    Updated styling for menu button

--- Scripts/menuToggle.js
+++ Scripts/menuToggle.js
@@ -6,5 +6,6 @@ function toggleMenu() {
     var menuDecor = document.getElementById("menuDecor"); 
     isActive = !isActive;
     menuDecor.classList.toggle("show");
+    document.querySelector(".MenuButton").classList.toggle("show");
     menu.classList.toggle("show");
 }
