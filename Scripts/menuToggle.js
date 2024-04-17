1cd7ad7209cb29ffdaedc31938df27ded546021f
toxikdnb <toxikdnb@hotmail.com>
2024-04-17 11:00:34 +0100

    Added menuButton rotation animation

--- Scripts/menuToggle.js
+++ Scripts/menuToggle.js
@@ -1,5 +1,10 @@
+let isActive = false;
+
 function toggleMenu() {
     var menuPanel = document.getElementById("menu");
     var menu = document.querySelector(".Menu");
+    var menuDecor = document.getElementById("menuDecor"); 
+    isActive = !isActive;
+    menuDecor.classList.toggle("show");
     menu.classList.toggle("show");
 }
