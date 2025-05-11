5168f4bbc1f53f0ceb026048140cd6e491ad3ae0
toxikdnb <toxikdnb@hotmail.com>
2025-05-11 15:18:08 +0100

    Added reset scroll script

---
+++ scripts/resetScroll.js
@@ -0,0 +1,11 @@
+if ("scrollRestoration" in history) {
+    history.scrollRestoration =
+        "manual";
+}
+
+window.addEventListener(
+    "load",
+    function () {
+        window.scrollTo(0, 0);
+    }
+);
